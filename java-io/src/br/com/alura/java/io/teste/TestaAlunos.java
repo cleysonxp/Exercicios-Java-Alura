be937c53d6f77ff7e1ae941b8894e3ef875a9e17
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2021-11-17 21:35:53 -0300

    Utilizando Set para lista

---
+++ java-io/src/br/com/alura/java/io/teste/TestaAlunos.java
@@ -0,0 +1,38 @@
+package br.com.alura.java.io.teste;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestaAlunos {
+	
+	public static void main(String[] args) {
+		
+		Set<String> alunos = new HashSet<>();
+		alunos.add("Paulo");
+        alunos.add("Alberto");
+        alunos.add("Pedro");    
+        alunos.add("Nico");
+        alunos.add("Nico");
+        
+        System.out.println(alunos.size());
+        
+        boolean existe = alunos.contains("Paulo");
+        System.out.println(existe);
+        
+        System.out.println();
+        
+        for (String aluno : alunos) {
+			System.out.println(aluno);
+		}
+        System.out.println();
+        
+        alunos.forEach(aluno ->{
+        	System.out.println(aluno);
+        });
+        
+        System.out.println();
+        System.out.println(alunos);
+		
+	}
+
+}
