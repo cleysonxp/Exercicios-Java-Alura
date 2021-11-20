ef365ef62bb3440ac0c23076f8b15a55f5a824b2
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2021-11-19 23:02:12 -0300

    Adicionando Set para alunos, métodos matricula e getAlunos, alteração do toString.

--- gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -2,14 +2,17 @@ package br.com.alura;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
 
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
+	private Set<Aluno> alunos = new HashSet<>();
 
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -39,6 +42,14 @@ public class Curso {
 //			tempoTotal += aula.getTempo();
 //		}
 //		return tempoTotal;
+	}
+	
+	public void matricula(Aluno aluno){
+	    this.alunos.add(aluno);
+	}
+	
+	public Set<Aluno> getAlunos() {
+	    return Collections.unmodifiableSet(alunos);
 	}
 	
 	 @Override
