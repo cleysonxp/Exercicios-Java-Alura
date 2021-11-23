63d3ee7779d8a79d2de5d1a9d6b72c09b00a16f4
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2021-11-22 21:10:44 -0300

    Utilização de métodos default e lambdas

--- java8/src/java8/OrdenaString.java
+++ java8/src/java8/OrdenaString.java
@@ -14,34 +14,45 @@ public class OrdenaString {
 		palavras.add("casa do código");
 		palavras.add("caelum");
 
-		ComparadorPorTamanho comparador = new ComparadorPorTamanho();
-		palavras.sort(comparador);
+//		ComparadorPorTamanho comparador = new ComparadorPorTamanho();
+		
+//		palavras.sort(comparador);
+		
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		
 		System.out.println(palavras);
 
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
+//		Consumer<String> consumidor = new ImprimeNaLinha();
+//		palavras.forEach(consumidor);
+
+		Consumer<String> consumidor = new Consumer<String>() {
+			public void accept(String s) {
+				System.out.println(s);
+			}
+		};
+		palavras.forEach(s -> System.out.println(s));
 	}
 }
 
-class ComparadorPorTamanho implements Comparator<String> {
+//class ComparadorPorTamanho implements Comparator<String> {
+//
+//	@Override
+//	public int compare(String s1, String s2) {
+//		if (s1.length() < s2.length())
+//			return -1;
+//		if (s1.length() > s2.length())
+//			return 1;
+//		return 0;
+//	}
+//
+//}
 
-	@Override
-	public int compare(String s1, String s2) {
-		if (s1.length() < s2.length())
-			return -1;
-		if (s1.length() > s2.length())
-			return 1;
-		return 0;
-	}
-
-}
-
-class ImprimeNaLinha implements Consumer<String> {
-
-	@Override
-	public void accept(String s) {
-		System.out.println(s);
-
-	}
-
-}
+//class ImprimeNaLinha implements Consumer<String> {
+//
+//	@Override
+//	public void accept(String s) {
+//		System.out.println(s);
+//
+//	}
+//
+//}
