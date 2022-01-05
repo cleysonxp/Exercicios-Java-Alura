5530350fa44d392eee2468eae2ec5fca53808162
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2022-01-04 21:22:56 -0300

    Formulario de login

--- gerenciadorM/src/br/com/alura/gerenciadorM/modelo/Banco.java
+++ gerenciadorM/src/br/com/alura/gerenciadorM/modelo/Banco.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class Banco {
 private static List<Empresa> lista = new ArrayList<>();
+private static List<Usuario> listaUsuarios = new ArrayList<>();
 private static Integer chaveSequencial = 1;
 	
 	static {
@@ -17,6 +18,17 @@ private static Integer chaveSequencial = 1;
 		empresa2.setId(chaveSequencial++);
 		lista.add(empresa);
 		lista.add(empresa2);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("mario");
+		u1.setSenha("12345");
+		
+		Usuario u2 = new Usuario();
+		u2.setLogin("mario");
+		u2.setSenha("12345");
+		
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
 	}
 
 	public void adiciona(Empresa empresa) {
@@ -46,6 +58,15 @@ private static Integer chaveSequencial = 1;
 				return empresa;
 			}
 		}
+		return null;
+	}
+
+	public Usuario existeUsuarios(String login, String senha) {
+		for(Usuario usuario : listaUsuarios) {
+			if(usuario.ehIgual(login, senha)) {
+				return usuario;
+			}
+		}	
 		return null;
 	}
 
